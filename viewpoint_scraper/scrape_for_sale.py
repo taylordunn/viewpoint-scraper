cee731c0ca821d611551a4ce89a628ee13f63155
taylordunn <t.dunn19@gmail.com>
2025-05-09 20:04:04 -0300

    trying new headless mode to fix listing history

--- viewpoint_scraper/scrape_for_sale.py
+++ viewpoint_scraper/scrape_for_sale.py
@@ -23,7 +23,7 @@ logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-gpu")
